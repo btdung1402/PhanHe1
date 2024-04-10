ffa87f9b9cdcfee5412cce75e336f32f6bdb79c5
btdung1402 <dungb.work1402@gmail.com>
2024-04-10 12:37:17 +0700

    Added DataProvider Class and tested view all users

---
+++ PhanHe1/DAO/DataProvider.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+
+namespace PhanHe1.DAO
+{
+    public class OracleDataProvider
+    {
+        private static OracleDataProvider instance;
+        private static readonly object lockObject = new object();
+
+        //You can change the default connection string in app.config
+        private static string _connectionString = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+      
+        private OracleDataProvider(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        private OracleDataProvider() { }
+
+        public static OracleDataProvider Instance
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new OracleDataProvider();
+                    }
+                    return instance;
+                }
+            }
+            private set => OracleDataProvider.instance = value;
+        }
+
+        public static void Initialize(string connectionString)
+        {
+            if (instance != null)
+            {
+                _connectionString = connectionString;
+            }
+            instance = new OracleDataProvider(connectionString);
+        }
+
+        public DataTable ExecuteQuery(string sql, object[] parameters = null)
+        {
+            DataTable data = new DataTable();
+
+            using (OracleConnection connection = new OracleConnection(_connectionString))
+            {
+                connection.Open();
+
+                OracleCommand command = new OracleCommand(sql, connection);
+
+                if (parameters != null)
+                {
+                    string[] listPara = sql.Split(' ');
+                    int i = 0;
+                    foreach (string s in listPara)
+                    {
+                        if (s.Contains(":"))
+                        {
+                            command.Parameters.Add(s, parameters[i++]);
+                        }
+                    }
+                }
+
+                OracleDataAdapter adapter = new OracleDataAdapter(command);
+                adapter.Fill(data);
+
+                connection.Close();
+            }
+
+            return data;
+        }
+
+        public int ExecuteNonQuery(string sql, object[] parameters = null)
+        {
+            int rowsAffected = 0;
+            using (OracleConnection connection = new OracleConnection(_connectionString))
+            {
+                connection.Open();
+
+                OracleCommand command = new OracleCommand(sql, connection);
+
+                // Add parameters if provided
+                if (parameters != null)
+                {
+                    string[] listPara = sql.Split(' ');
+                    int i = 0;
+                    foreach (string s in listPara)
+                    {
+                        if (s.Contains(":"))
+                        {
+                            command.Parameters.Add(s, parameters[i++]);
+                        }
+                    }
+                }
+
+                rowsAffected = command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+
+            return rowsAffected;
+        }
+
+        public object ExecuteScalar(string sql, object[] parameters = null)
+        {
+            object data = null;
+            using (OracleConnection connection = new OracleConnection(_connectionString))
+            {
+                connection.Open();
+
+                OracleCommand command = new OracleCommand(sql, connection);
+
+                if (parameters != null)
+                {
+                    string[] listPara = sql.Split(' ');
+                    int i = 0;
+                    foreach (string s in listPara)
+                    {
+                        if (s.Contains(":"))
+                        {
+                            command.Parameters.Add(s, parameters[i++]);
+                        }
+                    }
+                }
+
+                data = command.ExecuteScalar();
+
+                connection.Close();
+            }
+            return data;
+        }
+
+        // Add more methods for specific database operations as needed
+
+    }
+
+}
--- PhanHe1/Form1.cs
+++ PhanHe1/Form1.cs
@@ -9,11 +9,12 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Oracle.ManagedDataAccess.Client; //Oracle Client Library
 using System.Configuration; //Access Application Config Attributes
+using PhanHe1.DAO;
+
 namespace PhanHe1
 {
     public partial class Form1 : Form
-    {
-        OracleConnection con = new OracleConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+    { 
         public Form1()
         {
             InitializeComponent();
@@ -21,10 +22,16 @@ namespace PhanHe1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            con.Open();
-            MessageBox.Show("Connected!");
-            con.Clone();
-
+            string query = "SELECT username FROM sys.dba_users";
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            try
+            {
+                dataGridView1.DataSource = OracleDataProvider.Instance.ExecuteQuery(query);
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
