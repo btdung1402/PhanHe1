6b2fd4746e7e9ff134917701c6c07cefe8095450
btdung1402 <dungb.work1402@gmail.com>
2024-04-10 09:33:29 +0700

    Created a test connection to oracle db

--- PhanHe1/Form1.cs
+++ PhanHe1/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Oracle.ManagedDataAccess.Client; //Oracle Client Library
+using System.Configuration; //Access Application Config Attributes
 namespace PhanHe1
 {
     public partial class Form1 : Form
     {
+        OracleConnection con = new OracleConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            MessageBox.Show("Connected!");
+            con.Clone();
+
         }
     }
 }
